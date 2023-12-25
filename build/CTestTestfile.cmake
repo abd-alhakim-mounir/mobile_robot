f94fdfea1766b0a92184ed38112ee2bcbb78c549
abd-alhakim-mounir <hakeemmouneer123@gmail.com>
2023-12-25 20:57:00 +0200

    Hellllp

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/hakeem/catkin2_ws/src
+# Build directory: /home/hakeem/catkin2_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("spcbot_navigation")
+subdirs("spcbot_description")
+subdirs("spcbot_gazebo")
---
+++ build/spcbot_navigation/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/hakeem/catkin2_ws/src/spcbot_navigation
+# Build directory: /home/hakeem/catkin2_ws/build/spcbot_navigation
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
